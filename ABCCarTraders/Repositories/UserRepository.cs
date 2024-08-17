87757fbaee1e432e2e58932b545c4c1e07d8444d
sajithapislk <sajith@apis.lk>
2024-08-17 19:23:35 +0530

    connected to mssql and working login

--- ABCCarTraders/Repositories/UserRepository.cs
+++ ABCCarTraders/Repositories/UserRepository.cs
@@ -1,6 +1,8 @@
 ﻿using ABCCarTraders.Models;
+using ABCCarTraders.Services;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,19 +11,31 @@ namespace ABCCarTraders.Repositories
 {
     class UserRepository
     {
-        private List<User> _users;
+        private readonly DatabaseService _dbService;
 
         public UserRepository()
         {
-            _users = new List<User>
-            {
-                new Admin { Id = 1, Username = "admin", Password = "admin123" },
-            };
+            _dbService = new DatabaseService();
         }
 
         public User GetUser(string username, string password)
         {
-            return _users.FirstOrDefault(u => u.Username == username && u.Password == password);
+            string query = $"SELECT * FROM users WHERE username = '{username}' AND password = '{password}'";
+            DataTable result = _dbService.ExecuteQuery(query);
+            if (result.Rows.Count > 0)
+            {
+                DataRow row = result.Rows[0];
+                if (Convert.ToBoolean(row["is_admin"]))
+                {
+                    return new Admin
+                    {
+                        Id = Convert.ToInt32(row["Id"]),
+                        Username = row["username"].ToString(),
+                        Password = row["password"].ToString()
+                    };
+                }
+            }
+            return null;
         }
     }
 }
---
+++ ABCCarTraders/Services/DatabaseService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABCCarTraders.Services
+{
+    public class DatabaseService
+    {
+        private readonly string _connectionString;
+
+        public DatabaseService()
+        {
+            _connectionString = @"Data Source=APISLK\SQLEXPRESS;Initial Catalog=ABCCarTraders;Persist Security Info=True;User ID=sa;Password=1235789";
+        }
+
+        public SqlConnection GetConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
+
+        public DataTable ExecuteQuery(string query)
+        {
+            using (SqlConnection connection = GetConnection())
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+                    {
+                        DataTable dataTable = new DataTable();
+                        adapter.Fill(dataTable);
+                        return dataTable;
+                    }
+                }
+            }
+        }
+
+        public int ExecuteNonQuery(string query)
+        {
+            using (SqlConnection connection = GetConnection())
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    return command.ExecuteNonQuery();
+                }
+            }
+        }
+
+    }
+}
